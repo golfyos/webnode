4b71f0cc924ca0972f011786b3f60edfa3d68180
golf <golf_cat1@hotmail.com>
2017-03-08 21:48:15 +0700

    First

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req, res){
+  res.sendFile(__dirname + "/public/Main.html");
+});
+
+module.exports = app;
+
+app.listen(3000,function(){
+  console.log("port 3000");
+});
