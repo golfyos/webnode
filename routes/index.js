fcb9170dea14769be00045886b5dd1908c9e2f71
golf <golf_cat1@hotmail.com>
2017-03-09 18:50:27 +0700

    finish route home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/',function(req, res){
+    res.sendFile(path.resolve(__dirname + "/../public/Main.html"));
 });
+
+router.post('/about',function(req,res){
+  if(req.body.menu == "about"){
+      alert("gg");
+      res.sendFile(path.resolve(__dirname+"/../public/About.html"));
+  }
+});
+
 
 module.exports = router;
