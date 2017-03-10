6b7aa8e7eb9b9bc6f658c85b1536343828b98176
golf <golf_cat1@hotmail.com>
2017-03-10 23:02:49 +0700

    fix bug

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,12 @@ router.post('/play',function(req,res){
   if(req.body.menu == "about"){
       res.sendFile(path.resolve(__dirname+"/../public/About.html"));
   }
-  else{
+  else if(req.body.menu=="newgame"){
     res.sendFile(path.resolve(__dirname+"/../public/web-app.html"));
+       /* fs.readFile(path.resolve(__dirname + "/../public/json/question.json"),"utf-8",function(err,data){
+            var json = JSON.parse(data);
+            res.render("index.ejs",{data:json});
+        });*/  
   }
 });
 
